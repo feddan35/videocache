968eb6780f8f88fe478af0f976d93a041536b9c0
maxw <maxw@extramsp.com>
2017-02-23 20:17:54 +0000

    main and endpoint

--- endpoint.py
+++ endpoint.py
@@ -1,5 +1,14 @@
+def make_eps(dc_lats, ep_cache_lts, reqs):
+  eps = []
+  n = len(dc_lats)
+  for i in range(n):
+    epcachelats = {}
+    for data in ep_cache_lts[i]:
+      epcachelats[data[0]] = data[1]
+    eps.apend((epcachelats, dc_lats[i], 
+
 class Endpoint(object):
-  def __init__(self, latencies):
+  def __init__(self, latencies, dclat, reqs):
     self.lat = {}
     for latency in latencies:
       
--- main.py
+++ main.py
@@ -3,5 +3,18 @@ if __name__ == "__main__":
   filename = sys.argv[1]
   no_vids, no_eps, no_reqs, no_caches, csize, dc_lats, vidsizes, ep_cache_lts, reqs = parser.parse(filename)
 
-  dc = Datacentre(vidsizes)
+  if no_vids != len(vidsizes):
+    print "The virtual number of videos is different to the declared number of videos!!!"
+
+  if no_eps != len(dc_lats):
+    print "The virtual number of endpoints is different from the declared number of endpoints!!!"
+
+  if no_reqs != len(reqs):
+    print "The virtual number of requests is different from the declared number of requests!!!"
+
+  dc = Datacentre(vidsizes)  
+  eps = endpoint.make_eps(dc_lats, ep_cache_lts, reqs)
+  cs = cache.make_caches(no_caches, csize)
+
   print map(lambda x: "{} {}\n".format(x.id, x.size),dc.videos)
+  
