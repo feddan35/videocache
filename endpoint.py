ab91468db3236aa076154267a95836b09452f33a
maxw <maxw@extramsp.com>
2017-02-23 20:27:03 +0000

    endpoint finished

--- endpoint.py
+++ endpoint.py
@@ -5,10 +5,17 @@ def make_eps(dc_lats, ep_cache_lts, reqs):
     epcachelats = {}
     for data in ep_cache_lts[i]:
       epcachelats[data[0]] = data[1]
-    eps.apend((epcachelats, dc_lats[i], 
+    reqs = {}
+    for data in reqs:
+      reqs[data[0]] = data[1]
+    eps.append(Endpoint(epcachelats, dc_lats[i], reqs))
+  return eps
 
 class Endpoint(object):
   def __init__(self, latencies, dclat, reqs):
-    self.lat = {}
-    for latency in latencies:
-      
+    self.lats = latencies
+    self.dclat = dclat
+    self.reqs = reqs
+
+if __name__ == "__main__":
+  print map(lambda x: x.lats, make_eps([1,2], [[[1,2],[2,3]],[[0,1],[0,0]]], [[[1,2],[2,3]],[[0,1],[0,0]]]))
