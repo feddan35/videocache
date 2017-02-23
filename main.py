6c0f5d68bae62a8d725063fe32c9913145e8e09f
maxw <maxw@extramsp.com>
2017-02-23 19:45:28 +0000

    creating datacentre

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 if __name__ == "__main__":
-  import sys, parser
+  import sys, parser, datacentre
   filename = sys.argv[1]
   no_vids, no_eps, no_reqs, no_caches, csize, dc_lats, vidsizes, ep_cache_lts, reqs = parser.parse(filename)
+
+  dc = Datacentre(vidsizes)
+  print map(lambda x: "{} {}\n".format(x.id, x.size),dc.videos)
