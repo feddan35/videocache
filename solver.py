b81dba2d9d5e75317c813f42872ab8f37c5da339
daniel <feher.dani46@gmail.com>
2017-02-24 11:45:24 +0100

    solver changes

--- solver.py
+++ solver.py
@@ -1,17 +1,20 @@
-def solve(dc,eps,caches):
+def solve(dc,eps,cache):
     pevc = [[[eps[i].reqs[j]*(eps[i].dclat - eps[i].lats[k]) for k in cache] for j in eps[i].reqs.keys()] for i in eps]
     rvc = [[sum(pevc[i,j]) for j in cache] for i in dc.vids]
-    pv = []
-    [max(rvc[i]) for i in dc.vidlen]
+    pv = [] 
     for i in dc.vids:
         maxv = rvc[i,0]
         maxi = 0
+        maxvid = i
         for j in cache:
             if(rvc[i,j]>maxv):
-                maxv = rvc[i,j]
-        pv.append([maxv,maxi])
-    sortedpv = sorted([pv[]/dc.vids[i].size for i in dc.vids])
+                maxval = rvc[i,j]
+                maxi = j
+                maxvid = i
+        pv.append([maxval,maxvid,maxi])
+    sortedpv = sorted([pv[i]/dc.vids[i].size for i in dc.vids], reverse = True)
     for i in sortedpv:
-        #if A[i] can be cached, cache
+        if cache[i[2]].size > dc.vids[i[1]].size
+            cache[i[2]].add_video(i[1])
     
-    return caches
+    return cache
