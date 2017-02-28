c82e55dae928a41c07b69c22229dfbd22a679c0f
Maximilian Wasylow <bamwasylow@gmail.com>
2017-02-28 12:38:10 +0000

    Improved project directory structure

--- datacentre.py
+++
@@ -1,8 +0,0 @@
-from video import Video
-import numpy as np
-
-class Datacentre(object):
-  def __init__(self, videos):
-    self.videos = []
-    for i, size in enumerate(videos):
-      self.videos.append(Video(i, size))
